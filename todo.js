a4aeba1cd8cbf3ac35e9b31e823c3cba9fc9b57a
Navishka Kularathna <navishka.randika@gmail.com>
2019-10-16 15:16:08 +0530

    Phase 1 done!

---
+++ todo.js
@@ -0,0 +1,19 @@
+var todos = [];
+
+var input = prompt("What would you like to do?");
+
+while(input !== "quit"){
+    //Handle input
+    if(input === "list"){
+        console.log(todos);
+    } else if(input === "new"){
+        //Ask for new todo
+        var newTodo = prompt("Enter new todo");
+        //Add to todos array
+        todos.push(newTodo);
+    }
+
+    //Ask again for a new input
+    input = prompt("What would you like to do?");
+}
+console.log("See you again!");
