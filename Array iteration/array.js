682539466da3bfdccce4151ec1c84de9ed474eee
Navishka Kularathna <navishka.randika@gmail.com>
2019-10-18 19:38:51 +0530

    Using another function

--- Array iteration/array.js
+++ Array iteration/array.js
@@ -3,3 +3,11 @@ var colors = ["red", "orange", "yellow", "green"];
 colors.forEach(function(giveColor){
     console.log("Inside the forEach " + giveColor);
 })
+
+function printColors(color){
+    console.log("********");
+    console.log(color)
+    console.log("********");
+}
+
+colors.forEach(printColors);
