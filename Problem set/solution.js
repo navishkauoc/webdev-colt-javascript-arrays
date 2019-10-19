ba29aee8328a9c78662c0967ce860ccbddaec624
Navishka Kularathna <navishka.randika@gmail.com>
2019-10-19 11:50:36 +0530

    printReverse() function

--- Problem set/solution.js
+++ Problem set/solution.js
@@ -1,1 +1,9 @@
-alert("Check");
+function printReverse(oriArray){
+    var num = oriArray.length;
+    for(var i=0; i<= oriArray.length; i++){
+        console.log(oriArray[num]);
+        num -= 1;
+    }
+}
+
+printReverse([1, 2, 3]);
