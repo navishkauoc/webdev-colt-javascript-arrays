ddf28d61e2d1711d95024d1655944f2690ddf584
Navishka Kularathna <navishka.randika@gmail.com>
2019-10-19 10:57:53 +0530

    Feedbacks added

--- ToDo list/todo.js
+++ ToDo list/todo.js
@@ -15,11 +15,13 @@ while(input !== "quit"){
         var newTodo = prompt("Enter new todo");
         //Add to todos array
         todos.push(newTodo);
+        console.log("Added Todo");
     } else if(input === "delete"){
         //Ask for index of todo to be deleted
         var index = prompt("Enter index of todo to delete");
         //Delete that todo
         todos.splice(index,1);
+        console.log("Deleted Todo");
     }
 
     //Ask again for a new input
