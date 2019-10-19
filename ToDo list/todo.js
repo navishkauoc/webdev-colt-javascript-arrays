cb1892e1a8c0e1aebdde2536192538b96b77f294
Navishka Kularathna <navishka.randika@gmail.com>
2019-10-19 11:09:51 +0530

    Code organized

--- ToDo list/todo.js
+++ ToDo list/todo.js
@@ -5,27 +5,39 @@ var input = prompt("What would you like to do?");
 while(input !== "quit"){
     //Handle input
     if(input === "list"){
-        console.log("**********")
-        todos.forEach(function(todo, i){
-            console.log(i + ": " + todo);
-        });
-        console.log("**********")
+        listTodos();
     } else if(input === "new"){
-        //Ask for new todo
-        var newTodo = prompt("Enter new todo");
-        //Add to todos array
-        todos.push(newTodo);
-        console.log("Added Todo");
+        addTodo();
     } else if(input === "delete"){
-        //Ask for index of todo to be deleted
-        var index = prompt("Enter index of todo to delete");
-        //Delete that todo
-        todos.splice(index,1);
-        console.log("Deleted Todo");
+        deleteTodo();
     }
 
     //Ask again for a new input
     input = prompt("What would you like to do?");
 }
 console.log("See you again!");
+
+function listTodos(){
+    console.log("**********");
+    todos.forEach(function(todo, i){
+        console.log(i + ": " + todo);
+    });
+    console.log("**********");
+}
+
+function addTodo(){
+    //Ask for new todo
+    var newTodo = prompt("Enter new todo");
+    //Add to todos array
+    todos.push(newTodo);
+    console.log("Added Todo");
+}
+
+function deleteTodo(){
+    //Ask for index of todo to be deleted
+    var index = prompt("Enter index of todo to delete");
+    //Delete that todo
+    todos.splice(index,1);
+    console.log("Deleted Todo");
+}
 
